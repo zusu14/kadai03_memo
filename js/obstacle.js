328d7648a1442b5f2f01398afb152f8ffcade012
zusu14 <kohei.sn711@gmail.com>
2025-06-04 20:58:48 +0900

    2025060_修正 ・背景追加 ・障害物追加

--- js/obstacle.js
+++ js/obstacle.js
@@ -24,11 +24,35 @@ class Obstacle {
   }
 
   adaptToCanvasSize(w, h) {
-    this.width = w * 0.05;
-    this.height = h * 0.1;
-    this.speed = w * 0.005;
+    // this.speed = w * 0.01;
     this.x = w;
-    this.y = h * 0.9 - this.height;
+
+    switch (this.kind) {
+      case "food":
+        this.width = w * 0.05;
+        this.height = h * 0.1;
+        this.y = h * 0.9 - this.height;
+        this.speed = w * 0.011;
+        break;
+      case "house":
+        this.width = w * 0.225;
+        this.height = h * 0.4;
+        this.y = h * 0.9 - this.height;
+        this.speed = w * 0.01; // backgroundと合わせる
+        break;
+      case "building":
+        this.width = w * 0.12;
+        this.height = h * 0.65;
+        this.y = h * 0.9 - this.height;
+        this.speed = w * 0.01; // backgroundと合わせる
+        break;
+      case "frog":
+        this.width = w * 0.05;
+        this.height = h * 0.1;
+        this.y = h * 0.9 - this.height;
+        this.speed = w * 0.011;
+        break;
+    }
   }
 
   update() {
@@ -40,7 +64,7 @@ class Obstacle {
       ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
     } else {
       ctx.fillStyle = this.kind === "food" ? "yellow" : "red";
-      ctx.fillRect(this.image, this.x, this.y, this.width, this.height);
+      ctx.fillRect(this.x, this.y, this.width, this.height);
     }
   }
 }
